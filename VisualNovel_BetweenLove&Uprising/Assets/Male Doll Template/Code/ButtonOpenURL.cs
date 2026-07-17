ee3416252e68aff9ef777a1e87e5b6c0432c7d1d
pieve <64799644+pieverodrigues@users.noreply.github.com>
2026-07-17 08:52:18 -0300

    Adição Backgrounds e CGs + Cadastro

---
+++ VisualNovel_BetweenLove&Uprising/Assets/Male Doll Template/Code/ButtonOpenURL.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonOpenURL : MonoBehaviour
+{
+    public string url = "http://unity3d.com/";
+
+    private Button button;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+
+        button.onClick.AddListener(OpenURL);
+    }
+
+
+    public void OpenURL()
+    {
+        Application.OpenURL(url);
+    }
+}
